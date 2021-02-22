46452f2aec43052500e28cfab7940779ed7be309
dean <deanraguso@gmail.com>
2021-02-23 09:57:34 +1100

    Initialized and populated map

--- main.js
+++ main.js
@@ -5,7 +5,18 @@ const hole = 'O';
 const fieldCharacter = '░';
 const pathCharacter = '*';
 
-//Initialize the map
-width = prompt("Enter map width: ")
-height = prompt("Enter map height: ")
+const elements = [hole, fieldCharacter, pathCharacter]
 
+//Initialize the map
+const width = prompt("Enter map width: ")
+const height = prompt("Enter map height: ")
+var map = new Array(width);
+for (i = 0; i < width; i++){
+    map[i] = new Array(height);
+}
+
+for (i = 0; i < width; i++) {
+    for (j = 0; j < height; j++){
+        map[i][j] = elements[Math.round(Math.random()*2)];
+    }
+}
